df32657a23ab3ffe8b5e1cb89f096cb06dadf17e
Deivid380 <soydeividrc@gmail.com>
2025-09-17 23:50:45 -0500

    Pasaporte abtract y  factory

---
+++ PasaporteFX/visual/src/main/java/co/edu/poli/modelo/PasaporteOrdinario.java
@@ -0,0 +1,9 @@
+package co.edu.poli.modelo;
+
+public class PasaporteOrdinario extends Pasaporte {
+
+    public PasaporteOrdinario(String id, String fechaExp, Titular titular, Pais pais) {
+        super(id, fechaExp, titular, pais);
+    }
+    
+}
